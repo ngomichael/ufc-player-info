a2f3552a88565e34555cadc7c5f28e870af38d1a
ngomichael <michael_ngo@live.com>
2017-09-22 17:11:46 -0700

    animated the rankings title, created a list of fighters by calling mapon the array of fighters

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -17,21 +17,30 @@ const frontPageStyles = {
     alignItems: 'center',
 };
 
-const rankingsUnderlineContainer = {
-
-};
-
-const rankings = {
+const rankingsAndUnderlineContainer = {
     position: 'relative',
-    left: '55%',
-    fontFamily: 'Open Sans',
-    fontWeight: '400',
-    fontSize: '30px'
+    left: '50%',
+    top: '100px'
 };
+
+function animateRankingsTitle(opacity) {
+    const rankingsTitle = {
+        position: 'relative',
+        // left: '5%',
+        letterSpacing: '5px',
+        fontFamily: 'Roboto Slab',
+        fontWeight: '400',
+        fontSize: '30px',
+        transition: 'all 1.5s ease',
+        opacity: opacity,
+    };
+
+    return rankingsTitle;
+}
 
 const outerUnderline = {
     position: 'relative',
-    left: '55%',
+    // left: '5%',
     width: '100%',
     backgroundColor:'white',
     height: '4px',
@@ -46,22 +55,34 @@ function animateUnderline(width) {
         backgroundColor:'black',
         height: '4px',
         borderRadius: '4px',
-        transition: 'all 1s ease'
+        transition: 'all 1.5s ease'
     };
 
     return innerUnderline;
 }
+
+const rankingsList = {
+    fontSize: '20px'
+};
+
+const fighters = ['Demetrious Johnson', 'Conor McGregor', 'Stipe Miocic', 'Max Holloway', 'Daniel Cormeir', 'Cody Garbrandt', 'Joanna Jedrzejczyk', 'Tyron Woodley', 'Dominick Cruz', 'Jose Aldo' ];
+const rankedFightersItems = fighters.map((fighter) =>
+    <li style={{margin: '15px 0'}}>{fighter}</li>
+);
 
 class FrontPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             startingWidth: '0',
+            startingOpacity: '0',
         };
     }
 
+
     componentDidMount() {
         setTimeout(function() { this.setState({startingWidth: 100}); }.bind(this), 1);
+        setTimeout(function() { this.setState({startingOpacity: 1}); }.bind(this), 1);
     }
 
     render() {
@@ -71,11 +92,14 @@ class FrontPage extends Component {
                     <div style={titleStyles}>
                         Pick a Fighter
                     </div>
-                    <div style={rankingsUnderlineContainer}>
-                        <div style={rankings}>Pound for Pound</div>
+                    <div style={rankingsAndUnderlineContainer}>
+                        <div style={animateRankingsTitle(this.state.startingOpacity)}>Pound for Pound</div>
                         <div style={outerUnderline}>
                             <div style={animateUnderline(this.state.startingWidth)}></div>
                         </div>
+                        <ol style={rankingsList}>
+                            {rankedFightersItems}
+                        </ol>
                     </div>
                 </div>
 
