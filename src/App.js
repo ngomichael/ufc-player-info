93bfcf983c6c2fc14dcc738585cd0b5ccfafe036
ngomichael <michael_ngo@live.com>
2018-05-08 13:33:39 -0700

    imported react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import Header from "./Header";
-
-const fighters = ["Bobby Knuckles", "Max Blessed Holloway", "El Cucuy"];
+import {
+  HashRouter as Router,
+  Switch,
+  Redirect,
+  Route
+} from "react-router-dom";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Header fighterName={fighters[1]} country="Australia" />
-      </div>
-    );
+    return <div />;
   }
 }
 
