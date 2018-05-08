ccef83c2fb3ca3ca548372162609b2edbcdb61c0
ngomichael <michael_ngo@live.com>
2018-05-08 13:37:21 -0700

    added file with routes

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  frontPage: "/",
+  fighterInfo: "/signup",
+  //Colon in react router means this path will match anything that starts with
+  main: "/channels/:chanName"
+};
